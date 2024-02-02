548c5720d93c67ebee11069fb6c4cb3e13960b13
amirali.qdi <amirali.ghaedi@gmail.com>
2024-02-02 18:04:57 +0330

    Multi-thread functionality

--- Src/Client.py
+++ Src/Client.py
@@ -1,16 +1,21 @@
 import socket
+import threading
 import time
 
 import Data
 
+RED = '\033[91m'
+GREEN = '\033[92m'
+RESET = '\033[0m'
+
 
 class Client:
 
-    def __init__(self, window_size, sequence_number, connection_type=socket.AF_INET, protocol_type=socket.SOCK_STREAM):
-        self.__connection_type = connection_type
+    def __init__(self, window_size, sequence_number, timeout=1.0, connection_type=socket.AF_INET,
+                 protocol_type=socket.SOCK_STREAM):
         self.__sequence_number = sequence_number
-        self.__protocol_type = protocol_type
-        self.socket = self.__initialize_socket()
+        self.socket = self.__initialize_socket(connection_type, protocol_type)
+        self.socket.settimeout(timeout)
         self.__connection_flag = False
         self.__buffer = []
         self.__window = []
@@ -18,56 +23,78 @@ class Client:
         self.__message_buffer = []
         self.__sending_index = 0
         self.__sequence_packet_counter = 0
+        self.__receiving_thread = threading.Thread(target=self.__read_cycle, daemon=True, name="R-thread")
+        self.__sending_thread = threading.Thread(target=self.__send_cycle, daemon=True, name="S-thread")
+        self.__manage_thread = threading.Thread(target=self.__manage_thread_start, name="M-thread")
+        self.connection_time = 0
 
-    def __initialize_socket(self):
-        s = socket.socket(self.__connection_type, self.__protocol_type)
+    def __initialize_socket(self, connection_type, protocol_type):
+        s = socket.socket(connection_type, protocol_type)
         return s
 
     def connect(self, ip_address, port):
         print(f"Trying to connect to {ip_address}:{port}")
-        self.socket.connect((ip_address, port))
-        self.__connection_flag = True
-        print(f"Connection successful")
+        try:
+            self.socket.connect((ip_address, port))
+            self.__connection_flag = True
+            print(f"Connection successful")
+            return
+        except socket.timeout:
+            time.sleep(1)
+            self.connect(ip_address, port)
 
     def start(self):
-        read_cycle_counter = 1
-        connection_time = 0
-        while self.__connection_flag:
-            if self.__sequence_packet_counter * self.__sequence_number:
-                self.__connection_flag = False
-                self.socket.close()
-                break
-            for i in range(self.__window_size):
-                print(f"t={connection_time} ", end=" ")
+        self.__manage_thread.start()
 
-                time.sleep(1)
-                self.__send_cycle()
-                if read_cycle_counter % self.__window_size == 0:
-                    self.__read_cycle()
-                connection_time += 1
-                read_cycle_counter += 1
+    def __manage_thread_start(self):
+
+        self.connection_time = time.time()
+        self.__update_window()
+
+        self.__sending_thread.start()
+        # self.__receiving_thread.start()
+
+    def close_connection(self):
+        self.__buffer.clear()
+        self.__window.clear()
+        self.__connection_flag = False
+        self.socket.close()
 
     def __update_window(self):
         for i in range(self.__window_size):
-            self.__window[i] = self.__buffer[(self.__window_index * self.__window_size) + self.__sending_index + i]
+            self.__window.append(
+                self.__buffer[(self.__sequence_number * self.__sequence_packet_counter) + self.__sending_index + i])
 
     def __send_cycle(self):
-        data = self.__window.pop(0)
-        print(f"Client trans data frame {self.__sending_index}")
-        self.socket.send(data.encode())
-        self.__sending_index += 1
-        # full window sent
-        if len(self.__window_size) == 0:
-            self.__window_index += 1
-            self.__update_window()
+        while self.__connection_flag:
+            sending_frame = self.__window.pop(0)
+            self.socket.send(sending_frame.output().encode())
+            print(
+                f"Client/{GREEN}Send{RESET} {self.__get_current_time():.1f} | trans data frame {sending_frame.id}")
+            self.__sending_index += 1
+            if len(self.__window) == 0:
+                print("window empt")
+                self.__sequence_packet_counter += 1
+                self.__update_window()
+            if (self.__sequence_packet_counter * self.__sequence_number) + self.__sending_index > len(self.__buffer):
+                print("ALL DATA TRANSMITTED")
+                self.__connection_flag = False
+                self.close_connection()
+            time.sleep(1)
+
+    def __get_current_time(self):
+        return time.time() - self.connection_time
 
     def __read_cycle(self):
-        message = self.socket.recv(1024).decode()
-        frame = Data.input_frame(message)
-        if frame.p == 1:
-            if frame.data == "RR":
-                self.__sending_index = frame.id
-        print(f"Client recv: {frame.data}/{frame.id}")
+        while self.__connection_flag:
+            try:
+                message = self.socket.recv(1024).decode()
+                frame = Data.input_frame(message)
+                self.__message_buffer.append(frame)
+                print(f"Client/{RED}Read{RESET} {self.__get_current_time():.1f} | recv: {frame.data}/{frame.id}")
+            except socket.timeout:
+                print(
+                    f"Client/{RED}Read{RESET} {self.__get_current_time():.1f} |Timeout: No data received from server.")
 
     def send_data(self, data):
         input_frames = Data.input_stream(data, self.__sequence_number, len(self.__buffer) + len(self.__window))
--- Src/Server.py
+++ Src/Server.py
@@ -1,18 +1,24 @@
 import socket
+import threading
 import time
 
 from Src import Data
 from Src.Data import *
 
+RED = '\033[91m'
+GREEN = '\033[92m'
+RESET = '\033[0m'
+
 
 class Server:
 
-    def __init__(self, ip_address, port, window_size, sequence_number, connection_type=socket.AF_INET,
+    def __init__(self, ip_address, port, window_size, sequence_number, timeout=1.0, connection_type=socket.AF_INET,
                  protocol_type=socket.SOCK_STREAM):
 
         self.connection, self.client_ip_address = None, None
         self.__connection_flag = False
-        self.__socket = None
+        self.__socket = self.__initialize_socket(connection_type, protocol_type, ip_address, port)
+        self.__socket.settimeout(timeout)
 
         self.__sequence_number = sequence_number
         self.__window_size = window_size
@@ -20,70 +26,102 @@ class Server:
         self.__buffer = []
         self.__message_frames = []
 
-        self.__initialize_socket(connection_type, protocol_type, ip_address, port)
+        self.__receiving_thread = threading.Thread(target=self.__read_cycle, name="R-thread")
+        self.__sending_thread = threading.Thread(target=self.__send_cycle, name="S-thread")
+        self.__manage_thread = threading.Thread(target=self.__manage_thread_start, name="M-thread")
+        self.connection_time = 0
 
     def __initialize_socket(self, connection_type, protocol_type, server_ip, server_port):
         s = socket.socket(connection_type, protocol_type)
         s.bind((server_ip, server_port))
-        self.__socket = s
+        return s
 
-    def listen(self):
-        self.__socket.listen()
-        print("Waiting for client to connect...")
-        self.connection, self.client_ip_address = self.__socket.accept()
-        print(f"Connected to {self.client_ip_address}")
-        self.__connection_flag = True
-        print("Server is listening")
-        self.start()
+    def listen(self, timeout=1.0):
+        try:
+            self.__socket.listen()
+            self.connection, self.client_ip_address = self.__socket.accept()
+            print(f"Connected to {self.client_ip_address}")
+            self.__connection_flag = True
+            print("Server is listening")
+            self.connection.settimeout(timeout)
+            return
+        except socket.timeout:
+            print("Waiting for client to connect...")
+            self.listen()
 
     def start(self):
-        connection_time = 0
+        self.__manage_thread.start()
+
+    def __manage_thread_start(self):
+        self.connection_time = time.time()
+        self.__receiving_thread.start()
+        # self.__sending_thread.start()
         while self.__connection_flag:
-            time.sleep(1)
-            print(f"t={connection_time}  ", end="")
-            self.__read_cycle()
-            self.__send_cycle()
-            connection_time += 1
+            pass
+
+    def __get_current_time(self):
+        return time.time() - self.connection_time
 
     def __read_cycle(self):
-        data = self.connection.recv(1024).decode()
-        frame = Data.input_frame(data)
-        suspected_frame_id = (len(self.__buffer) + len(self.__window)) % self.__sequence_number
-        # data not received TODO
-        if frame.p == -1:
-            print("No data received form client")
-        # wrong frame type
-        elif frame.p == 1:
-            print("Wrong frame type from client, p bit was 1")
-        # window id match suspected id
-        elif frame.id == suspected_frame_id:
-            print(f"Server recv data frame {frame.id}")
-            self.__window.append(frame)
-            suspected_frame_id += 1
-        # window id does not match suspected id TODO
-        else:
-            srej_frame = Frame("REJ", suspected_frame_id, 1)
-            self.__message_frames.append(srej_frame)
+        counter = 0
+        while self.__connection_flag:
+            if len(self.__window) == 0 and len(self.__buffer) == 0:
+                suspected_frame_id = 0
+            elif len(self.__window) == 0 and len(self.__buffer) != 0:
+                suspected_frame_id = self.__buffer[-1].id + 1
+            else:
+                suspected_frame_id = self.__window[-1].id + 1
+            try:
+                data = self.connection.recv(1024).decode()
+                frame = Data.input_frame(data)
+            except socket.timeout:
+                print(
+                    f"Server/{RED}Read{RESET} {self.__get_current_time():.1f} | Timeout: No data received from client. {counter}")
+                continue
+            print(f"recv frame :{data}, wanted:{suspected_frame_id}| {counter}")
+            counter += 1
+            # data not received TODO
+            if frame.p == -1:
+                print(
+                    f"Server/{RED}Read{RESET} {self.__get_current_time():.1f} | (OLD ERROR)")
+                pass
+            # Inst FRAME
+            elif frame.p == 1:
+                pass
+            # TODO
+            # window id match suspected id
+            elif frame.id == suspected_frame_id:
+                print(f"Server/{RED}Read{RESET} {self.__get_current_time():.1f} | recv data frame {frame.id}")
+                self.__window.append(frame)
+                suspected_frame_id += 1
+            # window id does not match suspected id TODO
+            else:
+                print("ERROR ADDED")
+                rej_frame = Frame("REJ", suspected_frame_id, 1)
+                self.__message_frames.append(rej_frame)
 
-        # window filled
-        if len(self.__window) == self.__window_size:
-            self.__add_window_to_buffer()
-            self.__window.clear()
-            rr_frame = Frame("RR", suspected_frame_id % self.__sequence_number, 1)
-            self.__message_frames.append(rr_frame)
+            # window filled
+            if len(self.__window) == self.__window_size:
+                self.__add_window_to_buffer()
+                self.__window.clear()
+                rr_frame = Frame("RR", suspected_frame_id % self.__sequence_number, 1)
+                self.__message_frames.append(rr_frame)
 
     def __add_window_to_buffer(self):
         for frame in self.__window:
             self.__buffer.append(frame)
 
     def __send_cycle(self):
-        if len(self.__message_frames) > 0:
-            data = self.__message_frames[0].output()
-            self.__message_frames.pop(0)
-            print(f"Server trans: {data}")
-            self.connection.sendall(data.encode())
-        else:
-            pass
+        while self.__connection_flag:
+            if len(self.__message_frames) > 0:
+                data = self.__message_frames[0].output()
+                self.__message_frames.pop(0)
+                self.connection.sendall(data.encode())
+                print(f"Server/{GREEN}Send{RESET} {self.__get_current_time():.1f} | trans: {data}")
+            else:
+                print(f"Server/{GREEN}Send{RESET} {self.__get_current_time():.1f} | trans: none")
+                pass
+            time.sleep(1)
 
     def __print_message_buffer(self):
         for frame in server.__message_frames:
@@ -92,3 +130,4 @@ class Server:
 
 server = Server("127.0.0.1", 8080, 2, 8)
 server.listen()
+server.start()
